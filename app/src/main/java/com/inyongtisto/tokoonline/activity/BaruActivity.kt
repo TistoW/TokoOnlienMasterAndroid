e55644464ddbebcd058d0ffdfd90f00d7dcd0487
Tisto wayudi <imtisto@gmail.com>
2020-07-01 08:53:22 +0700

    Tutorial 7: ScrollView

---
+++ app/src/main/java/com/inyongtisto/tokoonline/activity/BaruActivity.kt
@@ -0,0 +1,13 @@
+package com.inyongtisto.tokoonline.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inyongtisto.tokoonline.R
+
+class BaruActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_baru)
+    }
+}
--- app/src/main/java/com/inyongtisto/tokoonline/fragment/HomeFragment.kt
+++ app/src/main/java/com/inyongtisto/tokoonline/fragment/HomeFragment.kt
@@ -22,6 +22,8 @@ class HomeFragment : Fragment() {
 
     lateinit var vpSlider: ViewPager
     lateinit var rvProduk:RecyclerView
+    lateinit var rvProdukTerlasir:RecyclerView
+    lateinit var rvElektronik:RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +33,8 @@ class HomeFragment : Fragment() {
 
         vpSlider = view.findViewById(R.id.vp_slider)
         rvProduk = view.findViewById(R.id.rv_produk)
+        rvProdukTerlasir = view.findViewById(R.id.rv_produkTerlasir)
+        rvElektronik = view.findViewById(R.id.rv_elektronik)
 
         val arrSlider = ArrayList<Int>()
         arrSlider.add(R.drawable.slider1)
@@ -43,8 +47,20 @@ class HomeFragment : Fragment() {
         val layoutManager = LinearLayoutManager(activity)
         layoutManager.orientation = LinearLayoutManager.HORIZONTAL
 
+        val layoutManager2 = LinearLayoutManager(activity)
+        layoutManager2.orientation = LinearLayoutManager.HORIZONTAL
+
+        val layoutManager3 = LinearLayoutManager(activity)
+        layoutManager3.orientation = LinearLayoutManager.HORIZONTAL
+
         rvProduk.adapter = AdapterProduk(arrProduk)
         rvProduk.layoutManager = layoutManager
+
+        rvProdukTerlasir.adapter = AdapterProduk(arrProdukTerlaris)
+        rvProdukTerlasir.layoutManager = layoutManager2
+
+        rvElektronik.adapter = AdapterProduk(arrElektronik)
+        rvElektronik.layoutManager = layoutManager3
 
         return view
     }
@@ -66,9 +82,75 @@ class HomeFragment : Fragment() {
         p3.harga = "Rp.14.200.000"
         p3.gambar = R.drawable.hp_pavilion_13_an0006na
 
+        val p4 = Produk()
+        p4.nama = "Hp Envy_13_aq0019tx"
+        p4.harga = "Rp.15.980.000"
+        p4.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
         arr.add(p1)
         arr.add(p2)
         arr.add(p3)
+        arr.add(p4)
+
+        return arr
+    }
+
+    val arrElektronik: ArrayList<Produk>get(){
+        val arr = ArrayList<Produk>()
+        val p1 = Produk()
+        p1.nama = "HP 14_bs749tu"
+        p1.harga = "Rp.5.500.000"
+        p1.gambar = R.drawable.hp_notebook_14_bs710tu
+
+        val p2 = Produk()
+        p2.nama = "Hp Envy_13_aq0019tx"
+        p2.harga = "Rp.15.980.000"
+        p2.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
+        val p3 = Produk()
+        p3.nama = "HP pavilion_13_an0006na"
+        p3.harga = "Rp.14.200.000"
+        p3.gambar = R.drawable.hp_pavilion_13_an0006na
+
+        val p4 = Produk()
+        p4.nama = "Hp Envy_13_aq0019tx"
+        p4.harga = "Rp.15.980.000"
+        p4.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
+        arr.add(p2)
+        arr.add(p3)
+        arr.add(p1)
+        arr.add(p4)
+
+        return arr
+    }
+
+    val arrProdukTerlaris: ArrayList<Produk>get(){
+        val arr = ArrayList<Produk>()
+        val p1 = Produk()
+        p1.nama = "HP 14_bs749tu"
+        p1.harga = "Rp.5.500.000"
+        p1.gambar = R.drawable.hp_14_bs749tu
+
+        val p2 = Produk()
+        p2.nama = "Hp Envy_13_aq0019tx"
+        p2.harga = "Rp.15.980.000"
+        p2.gambar = R.drawable.hp_pavilion_15_cx0056wm
+
+        val p3 = Produk()
+        p3.nama = "HP pavilion_13_an0006na"
+        p3.harga = "Rp.14.200.000"
+        p3.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
+        val p4 = Produk()
+        p4.nama = "Hp Envy_13_aq0019tx"
+        p4.harga = "Rp.15.980.000"
+        p4.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
+        arr.add(p4)
+        arr.add(p1)
+        arr.add(p3)
+        arr.add(p2)
 
         return arr
     }
