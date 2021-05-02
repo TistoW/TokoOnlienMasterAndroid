8fd784c1fec8eff355ee96889925bedf03096997
Inyong Tisto <imtisto@gmail.com>
2021-05-03 02:47:12 +0700

    Tutorial 48 Push Notif part1

--- app/src/main/java/com/inyongtisto/tokoonline/MainActivity.kt
+++ app/src/main/java/com/inyongtisto/tokoonline/MainActivity.kt
@@ -6,12 +6,16 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.messaging.FirebaseMessaging
 import com.inyongtisto.tokoonline.activity.LoginActivity
 import com.inyongtisto.tokoonline.activity.MasukActivity
 import com.inyongtisto.tokoonline.fragment.AkunFragment
@@ -46,6 +50,21 @@ class MainActivity : AppCompatActivity() {
         setUpBottomNav()
 
         LocalBroadcastManager.getInstance(this).registerReceiver(mMessage, IntentFilter("event:keranjang"))
+
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("Respon", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d("respon fcm:", token.toString())
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
     }
 
     val mMessage: BroadcastReceiver = object : BroadcastReceiver() {
---
+++ app/src/main/java/com/inyongtisto/tokoonline/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,41 @@
+package com.inyongtisto.tokoonline.firebase
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    val TAG = "RESPON PUSH NOTIF"
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+//                scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+//                handleNow()
+            }
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}  - ${it.title}")
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+}
